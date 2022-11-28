8f5109e30d0fb535f146e49a299e20345c88e127
Raharinda <raharindanadhif@gmai.com>
2022-11-28 14:24:44 +0700

    6

--- ic.js
+++ ic.js
@@ -1,4 +1,12 @@
+// DARK MODE
 function changeMode() {
    var element = document.body;
    element.classList.toggle("dark-mode");
 }
+// NAVBAR SLIDE
+const menuToggle = document.querySelector('.menu-toggle input')
+const nav = document.querySelector('nav ul');
+
+menuToggle.addEventListener('click', function(){
+   nav.classList.toggle('slide');
+});
