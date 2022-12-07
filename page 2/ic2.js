93a6ff7313845a1552119d79bd6a8baa86a6dd3e
Raharinda <raharindanadhif@gmai.com>
2022-12-07 18:12:22 +0700

    edfd

--- page 2/ic2.js
+++ page 2/ic2.js
@@ -5,3 +5,18 @@ const nav = document.querySelector('nav ul');
 menuToggle.addEventListener('click', function(){
    nav.classList.toggle('slide');
 });
+// STORAGE
+const form = document.querySelector('form');
+
+
+form.addEventListener('submit', (e) => {
+   e.preventDefault();
+
+   const fd = new FormData (form);
+   const obj = Object.fromEntries(fd);
+    
+    const json = JSON.stringify(obj);
+    localStorage.setItem('form', json);
+ 
+    window.location.href = "index3.html"
+})
